46032d707b7ae2fb1b1d6667e2eddd494fe9403a
thepixelflux <prishamaurya@gmail.com>
2026-07-05 13:20:26 +0530

    Fix model filenames casing and update API baseURL to Render

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://routemate-i35b.onrender.com",
 });
 
 // Interceptor to automatically add jwt token to headers if it exists
